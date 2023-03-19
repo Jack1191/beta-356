1c18422622658e38f75132a258a87a61eaf4e4c1
hp <hp@LAPTOP-I8BFUP9C>
2023-03-19 15:38:29 +0530

    Number

---
+++ src/New_Practice/Number.java
@@ -0,0 +1,14 @@
+package New_Practice;
+
+public class Number {
+	public static void main(String[] args) {
+		int i = 020;
+		int j = 20;
+		int k = 0X20;
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+	}
+
+}
