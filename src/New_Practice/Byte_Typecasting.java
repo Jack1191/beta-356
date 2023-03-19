9fa2e3d1cdba959a5bdc2eec77912a8e93ce3f33
hp <hp@LAPTOP-I8BFUP9C>
2023-03-19 15:26:29 +0530

    Typecasting

---
+++ src/New_Practice/Byte_Typecasting.java
@@ -0,0 +1,21 @@
+package New_Practice;
+
+public class Byte_Typecasting {
+	public static void main(String[] args) {
+		int a1 = 250;
+		int a2 = 120;
+		int a3 = 130;
+		int a4 = 300;
+			
+		byte b1 = (byte)a1;
+		byte b2 = (byte)a2;
+		byte b3 = (byte)a3;
+		byte b4 = (byte)a4;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+	}
+
+}
